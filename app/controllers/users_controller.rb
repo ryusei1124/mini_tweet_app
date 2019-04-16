b1b7b4a27f2771da7416ffd0a820e50959d2c336
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-04-16 14:55:53 +0000

    [6]終了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = "ユーザーを新規登録しました！"
+      redirect_to user_url @user
+    else
+      render :new
+    end
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました。"
+      redirect_to user_url @user
+    else
+      render :edit
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   root 'home#top'
   get 'top', to: 'home#top'
   get 'about', to: 'home#about'
@@ -9,4 +11,7 @@ Rails.application.routes.draw do
   get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
   post 'posts/:id/update', to: 'posts#update', as: :update_post
   post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
+  
+  resources :posts
+  resources :users
 end
