f0696d935ad3a0bd88deeb3593a60c39e560d56e
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-04-16 07:05:48 +0000

    [5]終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,38 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
+    if @post.save
+      flash[:notice] = "投稿しました!"
+      redirect_to posts_index_url
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to posts_index_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました!"
     redirect_to posts_index_url
   end
 end
