692004af497b4daace14c961d94f183ac26a277a
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-04-17 02:14:27 +0000

    [7]終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(name: params[:name], email: params[:email], image: "default.png",)
     if @user.save
       flash[:notice] = "ユーザーを新規登録しました！"
       redirect_to user_url @user
@@ -29,6 +29,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    if params[:image]
+      @user.image = "user_#{@user.id}.png"
+      File.binwrite("public/user_images/#{@user.image}", params[:image].read)
+    end
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました。"
       redirect_to user_url @user
